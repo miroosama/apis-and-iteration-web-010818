37e2dd41e9509cd0f77c0132ffaaa4f0b47a84b1
Osama Miro <o.miro650@gmail.com>
2018-01-09 15:27:54 -0500

    done again

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,30 +2,29 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
-  #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  found_character = character_hash["results"].find do |person|
-    person["name"] == character
-        end
-        character_films = found_character["films"]
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
-
+def get_characters(n)
+  all_characters = RestClient.get("http://www.swapi.co/api/people/?page=#{n}")
+  characters_hash = JSON.parse(all_characters)
 end
 
+def find_character(char)
+  found_char = nil
+  for num in 1..9
+  char_hash = get_characters(num)
+  char_hash["results"].each do |person|
+    if person['name'] == char
+    return person
+    end
+  end
+end
+puts  "Sorry, character doesn't match"
+end
 
-
+def get_character_movies_from_api(character)
+  found_character = find_character(character)
+  character_films = found_character["films"]
+end
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
   movies_arr = []
   films_hash.map do |link|
     movie = RestClient.get("#{link}")
@@ -34,17 +33,12 @@ def parse_character_movies(films_hash)
   end
   movies_arr.each do |movie|
     puts movie["title"]
+  end
 end
-end
-
-
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
-
-binding.pry
 ## BONUS
-
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
